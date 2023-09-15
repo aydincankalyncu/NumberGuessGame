a0502683328fd9775b61bec8293f20ec44f82956
Aydıncan Kalyoncu <aydincan.kalyoncu@arvento.onmicrosoft.com>
2023-09-15 22:34:41 +0300

    Desing part for start game screen.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartGameScreen from './src/screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <View style={styles.appContainer}>
+      <StartGameScreen/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  appContainer:{
+    flex: 1
+  }
 });
